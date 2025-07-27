9563def0d87fdf81d169bb5093057d15b131259e
Pure Git <gitpure12@gmail.com>
2025-07-27 14:47:12 +0000

    fix : studio-5393904285:~/studio{main}$ npm test
    > nextn@0.1.0 test
    > j

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,16 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
     '/node_modules/'
   ],
   moduleNameMapper: {
-    // CSS and static files
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '@/(.*)': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
   globals: {
